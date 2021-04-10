d648b75db3e786143b65cbf3052fa16002a77132
igrauwelman <igrauwelman@uni-osnabrueck.de>
2021-04-10 13:42:18 +0200

    added ContainerCollectable and PowerUp Behaviour

--- Assets/Scripts/PowerUpCollectable.cs
+++ Assets/Scripts/PowerUpCollectable.cs
@@ -1,18 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerUpCollectable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private float _speed = 2f;
+    
     void Update()
     {
-        
+        // let it move downwards
+        transform.Translate(Vector3.down * (_speed * Time.deltaTime));
+        // destroy if out of screen
+        if (transform.position.y < -1.6f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.ActivatePowerUp(this.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,11 +6,15 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject _cookiePrefab;
     [SerializeField] private List<GameObject> _trashPrefabs;
+    [SerializeField] private List<GameObject> _collectablePrefabs;
     [SerializeField] private float _delay = 3f;
+    [SerializeField] private float _collectableSpawnRate = 15f;
     private bool _spawningOn = true;
+    
     void Start()
     {
         StartCoroutine(SpawnSystem());
+        StartCoroutine(SpawnCollectables());
     }
     
     public void ONPlayerDeath()
@@ -28,6 +32,16 @@ public class SpawnManager : MonoBehaviour
             Instantiate(_trashPrefabs[(int) Random.Range(0f, _trashPrefabs.Count)], new Vector3(Random.Range(-8.3f, 8.3f), y: 10f, z: 0f), Quaternion.identity, this.transform);
 
             yield return new WaitForSeconds(_delay);
+        }
+    }
+
+    IEnumerator SpawnCollectables()
+    {
+        while (_spawningOn)
+        {
+            Instantiate(_collectablePrefabs[(int) Random.Range(0f, _collectablePrefabs.Count)],
+                new Vector3(Random.Range(-9f, 9f), 6.5f, 0f), Quaternion.identity, this.transform);
+            yield return new WaitForSeconds(_collectableSpawnRate);
         }
     }
 }
